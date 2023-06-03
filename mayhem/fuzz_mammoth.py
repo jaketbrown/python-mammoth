4799bb9881be4708482827de3d355eaf6935c02c
jake <jake@MayhemVM.myguest.virtualbox.org>
2023-06-03 13:59:58 -0400

    Initial Mayhem commit

---
+++ mayhem/fuzz_mammoth.py
@@ -0,0 +1,19 @@
+import atheris
+import sys
+import fuzz_helpers
+
+
+with atheris.instrument_imports():
+    import mammoth
+
+def TestOneInput(data):
+    fdp = fuzz_helpers.EnhancedFuzzedDataProvider(data)
+    mammoth.convert_to_html(fdp.ConsumeRemainingBytes())
+
+def main():
+    atheris.Setup(sys.argv, TestOneInput)
+    atheris.Fuzz()
+
+
+if __name__ == "__main__":
+    main()
